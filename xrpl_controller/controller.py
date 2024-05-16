25ec3afc492e685cc4020d30145345af3ba5be70
Lucas Witte <L.C.Witte@student.tudelft.nl>
2024-05-16 14:27:03 +0200

    Fix pipeline

--- xrpl_controller/controller.py
+++ xrpl_controller/controller.py
@@ -1,3 +1,14 @@
-# The packet received here are the raw bytes from the sslstream, which can be decoded with the ripple protobuffer.
+"""This module is responsible for handling the incoming packets from the interceptor."""
+
+
 def handle_packet(packet):
+    """
+    The packet received here are the raw bytes from the sslstream, which can be decoded with the ripple protobuffer.
+
+    Args:
+        packet: intercepted sslstream
+
+    Returns: None
+
+    """
     print(f"Packet received: {packet}")
--- xrpl_controller/packet_server.py
+++ xrpl_controller/packet_server.py
@@ -1,3 +1,5 @@
+"""This module is responsible for receiving the incoming packets from the interceptor and returning a response."""
+
 from concurrent import futures
 
 import grpc
@@ -6,17 +8,36 @@ from xrpl_controller import controller
 
 
 class PacketService(packet_pb2_grpc.PacketServiceServicer):
+    """This class is responsible for receiving the incoming packets from the interceptor and returning a response."""
+
     def SendPacket(self, request, context):
-        # Pass intercepted packet to controller
+        """
+        This function receives the packet from the interceptor and passes it to the controller.
+
+        Args:
+            request: intercepted sslstream
+            context: grpc context
+
+        Returns: acknoldgement message (This needs to be changed to a more meaningful response)
+
+        """
         controller.handle_packet(request.data)
         return packet_pb2.PacketAck(message="Packet received successfully")
 
+
 def serve():
+    """
+    This function starts the server and listens for incoming packets.
+
+    Returns: None
+
+    """
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     packet_pb2_grpc.add_PacketServiceServicer_to_server(PacketService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     serve()
