dcdf29a394574f57d078a2aade48315a341786be
Thijs van der Valk <t.c.j.vandervalk@student.tudelft.nl>
2024-05-22 15:50:11 +0200

    Removed print statement. Changed percentages to probabilities.

--- xrpl_controller/random_fuzzer.py
+++ xrpl_controller/random_fuzzer.py
@@ -14,9 +14,9 @@ class RandomFuzzer(Strategy):
 
     def __init__(
         self,
-        drop_percentage: float,
-        send_percentage: float,
-        delay_percentage: float,
+        drop_probability: float,
+        send_probability: float,
+        delay_probability: float,
         min_delay_ms: int,
         max_delay_ms: int,
     ):
@@ -24,18 +24,18 @@ class RandomFuzzer(Strategy):
         Initializes the random fuzzer.
 
         Args:
-            drop_percentage: percent of packages that will be dropped.
-            send_percentage: percent of packages that will be sent immediately.
-            delay_percentage: percent of packages that will be delayed.
+            drop_probability: percent of packages that will be dropped.
+            send_probability: percent of packages that will be sent immediately.
+            delay_probability: percent of packages that will be delayed.
             min_delay_ms: minimum number of milliseconds that will be delayed.
             max_delay_ms: maximum number of milliseconds that will be delayed.
         """
-        if (drop_percentage + send_percentage + delay_percentage) != 1:
-            raise ValueError("All percentages added must be equal to 1.")
+        if (drop_probability + send_probability + delay_probability) != 1:
+            raise ValueError("All probabilities added must be equal to 1.")
 
-        self.drop_percentage = drop_percentage
-        self.send_percentage = send_percentage
-        self.delay_percentage = delay_percentage
+        self.drop_probability = drop_probability
+        self.send_probability = send_probability
+        self.delay_probability = delay_probability
         self.min_delay_ms = min_delay_ms
         self.max_delay_ms = max_delay_ms
 
@@ -50,9 +50,9 @@ class RandomFuzzer(Strategy):
         Tuple[bytes, int]: the new packet and the random action.
         """
         choice: float = random.random()
-        if choice < self.send_percentage:
+        if choice < self.send_probability:
             return packet, 0
-        elif choice < self.send_percentage + self.drop_percentage:
+        elif choice < self.send_probability + self.drop_probability:
             return packet, MAX_U32
         else:
             return packet, random.randint(self.min_delay_ms, self.max_delay_ms)
