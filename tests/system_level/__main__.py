57f55724140da6c8c421aa3c174ea2be55d82d3c
Ivar van Loon <I.S.vanLoon@student.tudelft.nl>
2024-05-28 16:21:45 +0200

    Add hash comparison logic

--- tests/system_level/__main__.py
+++ tests/system_level/__main__.py
@@ -17,6 +17,17 @@ from git import Repo
 from shutil import rmtree
 
 
+async def fetch_node_hashes(ws_port: int):
+    async with AsyncWebsocketClient(f"ws://localhost:{ws_port}") as client:
+        info = ServerInfo()
+        ledger_response = await client.request(info)
+        if not ledger_response.is_successful():
+            print("request failed")
+            return None
+        await client.close()
+        return ledger_response.result.get('info').get('validated_ledger').get('hash')
+
+
 async def main():
     """Entry point of the system-level test suite."""
     strategy: RandomFuzzer = RandomFuzzer(0, 0, 1, 150)
@@ -24,8 +35,8 @@ async def main():
     t = Thread(target=serve, args=(strategy,))
     t.start()
 
-    # Repo.clone_from("git@gitlab.ewi.tudelft.nl:cse2000-software-project/2023-2024/cluster-q/13d/xrpl-packet"
-    #                 "-interceptor.git", ".temp-interceptor")
+    Repo.clone_from("git@gitlab.ewi.tudelft.nl:cse2000-software-project/2023-2024/cluster-q/13d/xrpl-packet"
+                    "-interceptor.git", ".temp-interceptor")
 
     build_process = subprocess.run(
         "cargo build",
@@ -52,17 +63,20 @@ async def main():
         )
         sleep(1)
 
-    async with AsyncWebsocketClient("ws://localhost:61000") as client:
-        info = ServerInfo()
-        ledger_response = await client.request(info)
-        while not ledger_response.is_successful():
-            print("request failed")
-            sleep(3)
-            ledger_response = await client.request(info)
-        print(
-            f"ledger response: {ledger_response.result.get('info').get('validated_ledger')}"
-        )
+    it = 0
+    while it < 50:
+        print("starting comparison")
+        tasks = [fetch_node_hashes(i.ws_public.port) for i in request_ledger_data.validator_node_list_store]
+        node_hashes = await asyncio.gather(*tasks)
 
+        temp = node_hashes[0]
+        for node_hash in node_hashes:
+            if node_hash != temp:
+                print(f"HASHES DIFFER {temp} {node_hash}")
+        it += 1
+        sleep(1)
+
+    print("test complete")
     (out, err) = run_process.communicate()
     print(out)
     print(err)
