0c43ec6417b3ffa933db6d56aaafcf4ed01c0ffb
Wishaal Kanhai <W.R.Kanhai@student.tudelft.nl>
2024-06-03 15:08:42 +0200

    Revert RandomFuzzer arguments

--- xrpl_controller/__main__.py
+++ xrpl_controller/__main__.py
@@ -5,6 +5,6 @@ from xrpl_controller.strategies import RandomFuzzer, Strategy
 from .packet_server import serve
 
 if __name__ == "__main__":
-    strategy: Strategy = RandomFuzzer(0, 0, 1, 150)
+    strategy: Strategy = RandomFuzzer(0.01, 0.04, 1, 150)
     serve(strategy)
     print("Controller Module started.")
