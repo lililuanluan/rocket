f6b5e4b6944d3751289b47ae1af5f93822faa4c6
Wishaal Kanhai <W.R.Kanhai@student.tudelft.nl>
2024-06-12 16:51:00 +0200

    Add test for process_packet

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -1,5 +1,9 @@
 """Tests for Strategy class."""
 
+from encodings.utf_8 import encode
+
+from protos import packet_pb2
+from xrpl_controller.core import MAX_U32
 from xrpl_controller.strategies import RandomFuzzer
 from xrpl_controller.validator_node_info import (
     SocketAddress,
@@ -63,3 +67,34 @@ def test_update_network():
             assert item.initial_message == b""
             assert item.final_message == b""
             assert item.action == -1
+
+
+def test_process_message():
+    """Test for process_message function."""
+    strategy = RandomFuzzer(0, 0.6, 10, 150, 10)
+    strategy.update_network([node_0, node_1, node_2])
+    packet_ack = packet_pb2.Packet(data=b"test", from_port=10, to_port=11)
+    assert strategy.process_packet(packet_ack) == (b"test", 119)
+
+    # Check whether action differs from previous one, could be flaky, but we used a seed
+    packet_ack = packet_pb2.Packet(data=b"test2", from_port=10, to_port=11)
+    assert strategy.process_packet(packet_ack) == (b"test2", 13)
+
+    # Check whether set_message gets modified
+    assert strategy.prev_message_action_matrix[0][1].initial_message == b"test2"
+    assert strategy.prev_message_action_matrix[0][1].action == 13
+    assert strategy.prev_message_action_matrix[0][1].final_message == b"test2"
+
+    # Check whether messages get dropped automatically through auto partition
+    strategy.partition_network([[10, 11], [12]])
+    for i in range(100):
+        msg = encode("test" + str(i))[0]  # Just arbitrary encoding
+        packet_ack = packet_pb2.Packet(data=msg, from_port=10, to_port=12)
+        assert strategy.process_packet(packet_ack) == (msg, MAX_U32)
+
+    # Check whether result will always stay the same with auto parse identical messages
+    packet_ack = packet_pb2.Packet(data=b"test", from_port=10, to_port=12)
+    result = strategy.process_packet(packet_ack)
+    for _ in range(100):
+        packet_ack = packet_pb2.Packet(data=b"test", from_port=10, to_port=12)
+        assert strategy.process_packet(packet_ack) == result
