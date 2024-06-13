a60b30029316c0a7c62458651b10ea3329fb1395
Wishaal Kanhai <W.R.Kanhai@student.tudelft.nl>
2024-06-13 13:27:51 +0200

    Fix format issue

--- tests/test_timestamp_microseconds.py
+++ tests/test_timestamp_microseconds.py
@@ -1,10 +1,12 @@
 """Tests for timestamp_microseconds method in core.py."""
+
 from datetime import datetime
 
 from xrpl_controller.core import timestamp_ms
 
 
 def test_timestamp_ms():
+    """Test timestamp_ms method."""
     # Checking whether timestamp is actually correct w.r.t. epoch is not testable for us.
     # We rely on the correctness of Python itself.
     time = datetime(2024, 1, 2, 3, 4, 5, 6)
