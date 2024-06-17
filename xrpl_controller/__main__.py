25e4903254b1980e9696391eceecc3c19fc29e9b
Ivar van Loon <I.S.vanLoon@student.tudelft.nl>
2024-06-17 11:53:13 +0200

    Fix mistake with import

--- xrpl_controller/__main__.py
+++ xrpl_controller/__main__.py
@@ -1,9 +1,8 @@
 """Entry point of the application, run with python -m xrpl_controller."""
 
+from xrpl_controller.packet_server import serve
 from xrpl_controller.strategies import Strategy
 from xrpl_controller.strategies.random_fuzzer import RandomFuzzer
-
-from xrpl_controller.packet_server import serve
 
 if __name__ == "__main__":
     strategy: Strategy = RandomFuzzer()
