a44221dae644e99b771635af1a688e6032200fd4
Lucas Witte <L.C.Witte@student.tudelft.nl>
2024-06-18 10:09:05 +0200

    add more assertions

--- tests/unit/test_packet_server.py
+++ tests/unit/test_packet_server.py
@@ -19,6 +19,8 @@ def test_send_packet_no_log():
     packet_ack = packet_server.send_packet(packet, None)
     assert packet_ack.data == b"test"
     assert packet_ack.action == 0
+    mock_strategy.process_packet.assert_called_once()
+
 
 def test_send_packet_with_log_no_logger():
     """Test the send_packet method of PacketService with logging but no existing logger."""
@@ -29,6 +31,8 @@ def test_send_packet_with_log_no_logger():
     packet_server = PacketService(mock_strategy)
     with pytest.raises(RuntimeError):
         packet_server.send_packet(packet, None)
+        mock_strategy.process_packet.assert_called_once()
+
 
 def test_send_packet_with_log_and_logger():
     """Test the send_packet method of PacketService with logging and an existing logger."""
@@ -38,11 +42,16 @@ def test_send_packet_with_log_and_logger():
     mock_strategy.keep_action_log = True
     packet_server = PacketService(mock_strategy)
     packet_server.logger = Mock()
-    with patch.object(PacketEncoderDecoder, "decode_packet", return_value=(Mock(), 61)):
+    with patch.object(
+        PacketEncoderDecoder, "decode_packet", return_value=(Mock(), 61)
+    ) as mock_decode_packet:
         packet_ack = packet_server.send_packet(packet, None)
+    assert mock_decode_packet.call_count == 2
     assert packet_ack.data == b"test"
     assert packet_ack.action == 0
     packet_server.logger.log_action.assert_called_once()
+    mock_strategy.process_packet.assert_called_once()
+
 
 def test_send_validator_node_info_no_log():
     """Test the send_validator_node_info method of PacketService without logging."""
@@ -52,6 +61,7 @@ def test_send_validator_node_info_no_log():
     request_iterator = [packet_pb2.ValidatorNodeInfo()]
     response = packet_server.send_validator_node_info(request_iterator, None)
     assert response.status == "Received validator node info"
+
 
 def test_send_validator_node_info_with_log():
     """Test the send_validator_node_info method of PacketService with logging without existing logger."""
@@ -70,7 +80,7 @@ def test_send_validator_node_info_with_existing_logger():
     mock_strategy.keep_action_log = True
     packet_server = PacketService(mock_strategy)
     mock_logger = Mock()
-    with patch('xrpl_controller.csv_logger.ActionLogger', return_value=mock_logger):
+    with patch("xrpl_controller.csv_logger.ActionLogger", return_value=mock_logger):
         packet_server.logger = mock_logger
         request_iterator = [packet_pb2.ValidatorNodeInfo()]
         response = packet_server.send_validator_node_info(request_iterator, None)
@@ -96,9 +106,13 @@ def test_get_config():
     response = packet_server.get_config(request, None)
     assert response.base_port_peer == mock_strategy.network_config.get("base_port_peer")
     assert response.base_port_ws == mock_strategy.network_config.get("base_port_ws")
-    assert response.base_port_ws_admin == mock_strategy.network_config.get("base_port_ws_admin")
+    assert response.base_port_ws_admin == mock_strategy.network_config.get(
+        "base_port_ws_admin"
+    )
     assert response.base_port_rpc == mock_strategy.network_config.get("base_port_rpc")
-    assert response.number_of_nodes == mock_strategy.network_config.get("number_of_nodes")
+    assert response.number_of_nodes == mock_strategy.network_config.get(
+        "number_of_nodes"
+    )
     assert list(response.partitions) == list(
         map(
             lambda x: packet_pb2.Partition(nodes=x),
