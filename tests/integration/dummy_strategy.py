31dd05512c7ff41dbbcc6351884841e8a27c77de
Lucas Witte <L.C.Witte@student.tudelft.nl>
2024-06-21 15:23:55 +0200

    improved doc

--- tests/integration/dummy_strategy.py
+++ tests/integration/dummy_strategy.py
@@ -1,11 +1,15 @@
-"""This is a dummy strategy that does nothing. It is used for testing purposes."""
+"""This module contains a dummy strategy. It is used for testing purposes."""
 
 from xrpl_controller.iteration_type import NoneIteration
 from xrpl_controller.strategies.strategy import Strategy
 
 
 class DummyStrategy(Strategy):
-    """Dummy strategy to be used for tests that allows to disable the available automatic processes in strategy."""
+    """
+    Dummy strategy to be used for tests.
+
+    It disables the available automatic processes in strategy as well as initializing the strategy with a NoneIteration.
+    """
 
     def __init__(self):
         """Initialize the dummy strategy."""
