da44038a992531800aafe39cd138357cc0eddb46
Thijs van der Valk <t.c.j.vandervalk@student.tudelft.nl>
2024-06-22 10:25:00 +0200

    Changed main

--- xrpl_controller/__main__.py
+++ xrpl_controller/__main__.py
@@ -6,7 +6,7 @@ from xrpl_controller.strategies import Strategy
 from xrpl_controller.strategies.random_fuzzer import RandomFuzzer
 
 if __name__ == "__main__":  # pragma: no cover
-    strategy: Strategy = RandomFuzzer(iteration_type=NoneIteration(timeout_seconds=30))
+    strategy: Strategy = RandomFuzzer()
     server = serve(strategy)
 
     server.wait_for_termination()
