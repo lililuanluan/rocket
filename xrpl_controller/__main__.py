62e8d868c5240393e59687221a2fb102480f1fe9
Thijs van der Valk <t.c.j.vandervalk@student.tudelft.nl>
2024-06-23 15:51:13 +0200

    Fixed failing pipeline 2nd commit

--- xrpl_controller/__main__.py
+++ xrpl_controller/__main__.py
@@ -1,6 +1,5 @@
 """Entry point of the application, run with python -m xrpl_controller."""
 
-from xrpl_controller.iteration_type import NoneIteration
 from xrpl_controller.packet_server import serve
 from xrpl_controller.strategies import Strategy
 from xrpl_controller.strategies.random_fuzzer import RandomFuzzer
