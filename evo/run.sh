74844f19f36b9c6c9c44d6bb8c5eb2b5c014502d
luanli <3352760396@qq.com>
2026-01-29 01:38:12 +0100

    clean up docker containers stuff

--- evo/run.sh
+++ evo/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ../.venv/bin/activate
-# python3 evotest_deap.py 2>&1 | tee evotest.log
-python3 evotest_deap.py
+python3 evotest_deap.py 2>&1 | tee evotest.log
+# python3 evotest_deap.py
