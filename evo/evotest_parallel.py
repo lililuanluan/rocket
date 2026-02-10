99a86b576907af9255ce358c8cc7573f95772ce9
luanli <3352760396@qq.com>
2026-02-10 09:21:34 +0100

    use my own fork of ripple for injecting bugs change makefile target names and not build all dockerfile for byzzfuzz bug reproduction

--- evo/evotest_parallel.py
+++ evo/evotest_parallel.py
@@ -192,7 +192,9 @@ def setup_docker_images(config):
 
     original_cwd = os.getcwd()
     os.chdir(ROCKET_DIR / "images")
-    subprocess.run(["make", "build"], check=True)
+    # 去掉ripple_image中前面 "xrpld:" 的部分
+    build_target = ripple_image.split(":")[-1]
+    subprocess.run(["make", build_target], check=True)
     print("✓ Local images built successfully")
     os.chdir(original_cwd)
 
--- images/setup.sh
+++ images/setup.sh
@@ -2,7 +2,6 @@
 
 
 if [ ! -d "./rippled" ]; then
-	git clone git@github.com:XRPLF/rippled.git
+	git clone git@github.com:lililuanluan/rippled.git
 fi
 
-docker build -f Dockerfile.rippled-1.4.0 -t xrpld:1.4.0-local .
