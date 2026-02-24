d728597b9c0883cd10a617124cec73121a5b5974
luanli <3352760396@qq.com>
2026-02-24 21:31:39 +0100

    try the new strategy

--- evo/run.sh
+++ evo/run.sh
@@ -3,4 +3,4 @@
 source ../.venv/bin/activate
 # python3 evotest_deap.py 2>&1 | tee evotest.log
 # python3 evotest_deap.py
-python3 evotest_parallel.py
+python3 evotest_parallel.py --strategy=EvoDelayBySeqStrategy
