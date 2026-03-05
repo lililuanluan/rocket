5e959d2b5163f1911a653255586d2c234061265f
luanli <3352760396@qq.com>
2026-03-05 14:35:40 +0100

    a single command to run evotest_parallel.py

--- evo/run.sh
+++ evo/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 source ../.venv/bin/activate
-# python3 evotest_deap.py 2>&1 | tee evotest.log
-# python3 evotest_deap.py
-python3 evotest_parallel.py --strategy=RandomDelayByzzPartitionStrategy # --max-parallel-workers=5
 
-# TODO: 修改路径配置逻辑，现在太混乱了
+python3 evotest_parallel.py --ripple-image xrpllabsofficial/xrpld:2.6.0 --min-delay-ms 0 --max-delay-ms 100 --rust-log-level info --byzz-min-seq 5 --byzz-max-seq 10  --max-ledger-seq 15 --total-num-tests 100 --population-size 5 --fitness-function "var_validation_time" --strategy "EvoDelayStrategy" --max-parallel-workers 1 --base-network-config-yaml "network.yaml" --mu 4 --timeout-per-seq 30 --individual-timeout-sec 300 --base-port-population 60000 --grpc-base-port 50051 --partition-seq 5 --partition-duration 3000  # --logs-group-dir 自动化
